8cffbe40f00d7a2599c5577c37c4deaf5890da35
Noemiegrau <noemiegrau@outlook.fr>
2021-04-24 16:54:42 -0700

    added Employee tests + Employee methods

---
+++ __test__/Intern.test.js
@@ -0,0 +1,3 @@
+class Intern extends Employee {
+    
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+// Packages
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateHTML = require('./src/generateHtmlPage');
+
+// Questions
