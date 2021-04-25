2d7d6ed9ee290a9e25da3754a77a09225289af1a
Noemiegrau <noemiegrau@outlook.fr>
2021-04-24 23:41:32 -0700

    added Intern passing tests and methods

--- __test__/Intern.test.js
+++ __test__/Intern.test.js
@@ -1,8 +1,21 @@
+const { expect } = require('@jest/globals');
 const Employee = require('../lib/Employee');
 const Intern = require('../lib/Intern');
 
-test('creates an Inten object', () => {
+test('creates an Intern object', () => {
     const intern = new Intern('Intern1');
 
-    expect(tyoeof(intern)).toBe();
+    expect(typeof(intern)).toBe('object');
+});
+
+test("gets Intern's school", () => {
+    const intern = new Intern('Intern1', '2', 'email', 'school1');
+
+    expect(intern.school).toBe('school1');
+});
+
+test('getRole() returns Intern', () => {
+    const intern = new Intern('Intern');
+
+    expect(intern.getRole()).toBe('Intern');;
 });
