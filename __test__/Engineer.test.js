6cb28eaf230887854580f40081a9ac6ec4330127
Noemiegrau <noemiegrau@outlook.fr>
2021-04-24 23:33:45 -0700

    added engineer passing tests and methods

--- __test__/Engineer.test.js
+++ __test__/Engineer.test.js
@@ -1,3 +1,21 @@
-class Engineer extends Employee {
-    
-}
+const Engineer = require('../lib/Engineer');
+const Employee = require('../lib/Employee');
+const { test } = require('@jest/globals');
+
+test('creates an engineer object', () => {
+    const engineer = new Engineer('Engineer1');
+
+    expect(typeof(engineer)).toBe('object');
+});
+
+test('gets github username', () => {
+    const engineer = new Engineer('Manager', '1', 'noemiegrau@outlook.fr', 'Noemiegithub');
+
+    expect(engineer.github).toBe('Noemiegithub');
+});
+
+test('getRole() returns Engineer', () => {
+    const engineer = new Engineer('Manager1');
+
+    expect(engineer.getRole()).toBe('Engineer');
+});
--- __test__/Intern.test.js
+++ __test__/Intern.test.js
@@ -1,3 +1,8 @@
-class Intern extends Employee {
-    
-}
+const Employee = require('../lib/Employee');
+const Intern = require('../lib/Intern');
+
+test('creates an Inten object', () => {
+    const intern = new Intern('Intern1');
+
+    expect(tyoeof(intern)).toBe();
+});
